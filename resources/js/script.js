45e6585b6f823b8043f8e2b2478c82daeeea52a0
Justice <justice.jares@gmail.com>
2021-07-07 14:25:34 -0700

    Made a lot of changes to improve the website

--- resources/js/script.js
+++ resources/js/script.js
@@ -42,6 +42,44 @@ $(document).ready(function() {
     });
 
     /* Animations on Scroll */
-    
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animate__animated animate__fadeIn');
+    }, {
+        offset: '50%'
+    });
 
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animate__animated animate__fadeInUp');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animate__animated animate__fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animate__animated animate__pulse');
+    }, {
+        offset: '50%'
+    });
+
+    /* MOBILE NAV */
+    $('.js--nav-icon').click(function () {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon ion-icon');
+
+        nav.slideToggle(200);
+        if (icon[0].name == 'reorder-four-outline') {
+            icon[0].name = 'close-circle-outline'
+        } else if (icon[0].name == 'close-circle-outline') {
+            icon[0].name = 'reorder-four-outline'
+        }
+
+
+    });
 });
+
+
